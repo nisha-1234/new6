63648c4484b50c143b3f57b3f5619657a59d7c6d
nisha-1234 <94951544+nisha-1234@users.noreply.github.com>
2022-01-05 09:00:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 #Declaring AWS Provider and credentials
 provider "aws" {
-  region     = "us-west-1"
+  region     = "us-east-1"
   access_key = "AKIAS7T2UNA2DZIOJBXL"
   secret_key = "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"
 }
 #######################################################
 #Creating Webserver
 resource "aws_instance" "webserver" {
- ami = "ami-01a9db914db1193ec"
+ ami = "ami-08895422b5f3aa64a"
  instance_type = "t2.micro"
  key_name = var.keyname
  vpc_security_group_ids = [var.linux_security_group]
@@ -27,7 +27,7 @@ output "Webserver_instance_id" {
 #######################################################
 #Creating Application
 resource "aws_instance" "application" {
- ami = "ami-05feb7439e86af1d9"
+ ami = "ami-0d80714a054d3360c"
  instance_type = "t2.micro"
  key_name = var.keyname
  vpc_security_group_ids = [var.windows_security_group]
@@ -46,7 +46,7 @@ output "Application_instance_id" {
 #######################################################
 #Creating db
 resource "aws_instance" "db" {
- ami = "ami-0bac45e8972d2593e"
+ ami = "ami-019c495c29e709719"
  instance_type = "t3.xlarge"
  key_name = var.keyname
  vpc_security_group_ids = [var.windows_security_group]
