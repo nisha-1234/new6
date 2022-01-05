c02b1feeffa460c065e53bf8a7024e588fd252c9
nisha-1234 <94951544+nisha-1234@users.noreply.github.com>
2022-01-05 09:24:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,41 +25,4 @@ output "Webserver_instance_id" {
   value = aws_instance.webserver.id
 }
 #######################################################
-#Creating Application
-resource "aws_instance" "application" {
- ami = "ami-0d80714a054d3360c"
- instance_type = "t2.micro"
- key_name = var.keyname
- vpc_security_group_ids = [var.windows_security_group]
- subnet_id = var.privatesubnetid
- 
- tags = {
-  Name = "application"
- }
-}
-output "Application_IP" {
-  value = aws_instance.application.public_ip
-}
-output "Application_instance_id" {
-  value = aws_instance.application.id
-}
-#######################################################
-#Creating db
-resource "aws_instance" "db" {
- ami = "ami-019c495c29e709719"
- instance_type = "t3.xlarge"
- key_name = var.keyname
- vpc_security_group_ids = [var.windows_security_group]
- subnet_id = var.privatesubnetid
- 
- tags = {
-  Name = "db"
- }
-}
-output "db_IP" {
-  value = aws_instance.db
-}
-output "db_instance_id" {
-  value = aws_instance.db.id
-}
-########################################################
+
